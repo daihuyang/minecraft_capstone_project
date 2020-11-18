8a678e10f13cbe848270c74a7f63b10ebe7b8958
ctakasakaubc <ctakasaka@alumni.ubc.ca>
2020-11-17 19:21:25 -0800

    fleshing out elements for ifttt

--- Electron/src/thenThat.ts
+++ Electron/src/thenThat.ts
@@ -1,7 +1,9 @@
 import * as hostedIDE from './hostedIDE';
 import * as constants from './base/sharedConstants.js';
 // is this import necessary?
-import { CompanionApp, ICompanionListener, getApp, CommandLogType } from './base/companionApp';
+import { getApp } from './base/companionApp';
+import { SocketServer } from './base/socketServer';
+
 
 // setting up app title
 const title = document.getElementById('TitleText');
@@ -16,10 +18,18 @@ hostedIDE.setupNavigationBar();
 // Need to pipe this into socketserver.ts or somehow connect the initialized CompanionApp
 // should be able to subscribe tto events using the compaion app's commandGlue interface
 
+// consolelog will not print
+// console.log("test");
+
+// testing a change in element HTML
+document.querySelector("#msg").innerHTML = "code compiled";
+// code will compile, however console cannot be accessed since it is pat of the companion app
+
+// cannot access the instance of the companionApp spawned by main.ts , but socketserver.ts can. Weird
+// getApp() will cause code to not compile at all, weird
 // getApp().debugLog("Test");
 // getApp().commandGlue.addEventSubscription('BlockPlaced', (response: any) => {
 //     getApp().debugLog(response.body.eventName);
 // });
-
 
 
