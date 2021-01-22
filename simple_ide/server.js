4caa2f90d3a3135cfd305ff6e348385a066d7683
ctakasakaubc <ctakasaka@alumni.ubc.ca>
2021-01-22 11:03:04 -0800

    Separating IFTTT UI, updating naming scheme

--- simple_ide/server.js
+++
@@ -1,9 +0,0 @@
-var static = require('node-static');
-var file = new static.Server('.');
-
-require('http').createServer(function (request, response) {
-	request.addListener('end', function () {
-		// serve files
-		file.serve(request, response);
-	}).resume();
-}).listen(3000);
