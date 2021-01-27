08105a2382927711c1d63e252c3e5d6a8b2c7cc0
ctakasakaubc <ctakasaka@alumni.ubc.ca>
2021-01-26 19:32:16 -0800

    Fixing folder structure for http.server

--- http_simple/python/serve.py
+++ http_simple/python/serve.py
@@ -2,8 +2,8 @@ import os
 from http.server import HTTPServer, CGIHTTPRequestHandler
 
 def run_server():
-    # Make sure the server is created at current directory
-    os.chdir('.')
+    # Make sure the server is pointed towards the correct directory
+    os.chdir('../static/')
     # Create server object listening the port 3000
     REPL_interface = HTTPServer(server_address=('', 3000), RequestHandlerClass=CGIHTTPRequestHandler)
     # Start the web server
