cea269949b9fddda2db3889c6c709c5fd953e326
MattsCurrie <matthewcurrie88@gmail.com>
2021-03-16 18:34:52 -0700

    added generator for reading events

--- react_simple/python/data_io.py
+++ react_simple/python/data_io.py
@@ -17,3 +17,31 @@ def write_event(file, event):
     with jsonlines.open(file, mode='a') as writer:
         writer.writer(event)
     
+def read_events(file):
+    '''
+    Yields events from file given
+
+    Params
+    ------
+    file : file-like object
+        JSON LINES file with game events stored in them
+    '''
+    with jsonlines.open(file, mode='r') as reader:
+        for event in reader:
+            yield event
+
+def clean_event(event):
+    '''
+    TODO Remove unwanted fields from JSON before storing
+
+    Params:
+    -------
+    event : str / JSON object
+        Event data received over websockets
+
+    Returns
+    -------
+    event : str / JSON
+        The event given with set of unnecessary fields removed
+    '''
+    pass
